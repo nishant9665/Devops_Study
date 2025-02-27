38e1330942a92b1b612d7502846283b3efc1b370
nishant9665 <nishant9665@gmail.com>
2025-02-27 19:44:10 +0530

    Python program test

---
+++ devops_test/dev_first.py
@@ -0,0 +1,17 @@
+
+def add():
+    a=10
+    b=20
+    c=a+b
+    print("This is addition ",c)
+    assert c == 30, "this is incorrect output"
+
+add()
+def sub():
+    a=10
+    b=20
+    c=a-b
+    print("This is subtractions ", c)
+    #assert c == 30, "this is incorrect output"
+sub()
+
